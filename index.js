c8edd8c5a5382b5be9269b6dcd1c9c5e242da2cb
Shamie <princessshamie@gmail.com>
2019-08-31 20:21:05 +0100

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('hello world')
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter the users year of birth\n', (year)=>{
+    age = 2019- year; 
+    if (isNaN(age)){
+        console.log('thats not a number')
+    }else if(age < 18  ){
+        console.log('the user is a minor')
+    }else if(18 <age> 36) {
+        console.log('the user is a youth')
+    }else
+        console.log('the rest are elders')
+    
+    rl.close()
+})
